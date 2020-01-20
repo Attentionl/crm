f819e079f7f69b2aca0a72edcf9236e0fe321085
yawen-code <2841254408@qq.com>
2020-01-20 10:50:51 +0800

    启动报 找不到mapper 警告

--- src/main/java/com/sxdx/CrmApplication.java
+++ src/main/java/com/sxdx/CrmApplication.java
@@ -2,8 +2,10 @@ package com.sxdx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.sxdx.basic.mapper")
 public class CrmApplication {
 
     public static void main(String[] args) {
