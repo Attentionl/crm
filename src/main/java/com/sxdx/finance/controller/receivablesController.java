0d10e1df21dcad071f891b39ff22135e17bea489
yawen-code <2841254408@qq.com>
2020-02-16 21:23:54 +0800

    应收账款的简单增删改查

---
+++ src/main/java/com/sxdx/finance/controller/receivablesController.java
@@ -0,0 +1,78 @@
+package com.sxdx.finance.controller;
+
+import com.sxdx.basic.bean.Receivadles;
+import com.sxdx.basic.utils.Message;
+import com.sxdx.basic.utils.MessageUtil;
+import com.sxdx.finance.service.IReceivablesService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import java.util.Date;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/receivables")
+public class receivablesController {
+
+  @Autowired
+  private IReceivablesService iReceivablesService;
+
+  @ApiOperation(value = "查询所有应收账款")
+  @GetMapping("SelectAll")
+  public Message findAll(){
+    List<Receivadles> list = iReceivablesService.selectAll();
+    return MessageUtil.success("查询成功","查询所有应收账款",list);
+  }
+
+
+  @ApiOperation(value = "通过id删除")
+  @GetMapping("deleteById")
+  public Message deleteById(Integer id){
+    iReceivablesService.deleteReceivablesById(id);
+    return MessageUtil.success("删除成功");
+  }
+
+
+  @ApiOperation(value = "批量删除")
+  @PostMapping("batchDelete")
+  public Message batchDelete(Integer[] ids){
+    iReceivablesService.batchDelete(ids);
+    return MessageUtil.success("删除成功");
+  }
+
+  @ApiOperation(value = "保存或更新")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name="amountReceivable",value = "应收金额",paramType = "form",required = true),
+      @ApiImplicitParam(name="receivableTime",value = "应收时间",paramType = "form",required = true),
+      @ApiImplicitParam(name="creater",value = "创建人员",paramType = "form",required = true),
+      @ApiImplicitParam(name="remark",value = "备注",paramType = "form"),
+  })
+  @PostMapping("saveOrUpdate")
+  public Message saveOrUpdate(
+      @NotNull Float amountReceivable,
+      Date receivableTime,
+      Integer creater,
+      String remark
+  ){
+    Receivadles receivadles = new Receivadles();
+    receivadles.setAmountreceivable(amountReceivable);
+    receivadles.setReceivabletime(receivableTime);
+    receivadles.setCreaterid(creater);
+    receivadles.setRemark(remark);
+    return MessageUtil.success("更新成功");
+  }
+
+
+
+}
+
+
---
+++ src/main/java/com/sxdx/finance/service/impl/IReceivablesServiceImpl.java
@@ -0,0 +1,82 @@
+package com.sxdx.finance.service.impl;
+
+import com.sxdx.basic.bean.Constract;
+import com.sxdx.basic.bean.Receivadles;
+import com.sxdx.basic.bean.ReceivadlesExample;
+import com.sxdx.basic.exception.CustomerException;
+import com.sxdx.basic.mapper.ReceivadlesMapper;
+import com.sxdx.finance.service.IReceivablesService;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @className IReceivablesServiceImpl
+ * @author yawen
+ * @version 1.0
+ * @description 应收账款业务实现类
+ * @createdTime 2020/2/16 10:38
+ */
+@Service
+public class IReceivablesServiceImpl implements IReceivablesService {
+
+  @Resource
+  private ReceivadlesMapper receivadlesMapper;
+
+  @Resource
+  private Constract constract;
+
+  @Override
+  public void saveOrUpdate(Receivadles receivadles) throws CustomerException {
+    if(receivadles.getReceivablesid() != null){
+      receivadlesMapper.updateByPrimaryKey(receivadles);
+    }else{
+      if(receivadles.getAmountreceivable() > constract.getAmount()){
+        throw new CustomerException("应收款金额不能大于合同金额！");
+      }
+      //id 是自增长，不用添加
+
+      //*没写：已收金额，是合同的已收金额；这个需要通过合同的回款信息求和计算
+
+      //未收金额=合同金额-已收金额
+      //创建时间是当前时间
+
+// 收款提醒：这个感觉有点麻烦啊
+//提醒人员
+//额外提醒
+
+      //合同编号应该在增加的时候已经获取到了，前端应该传入@RequestBody
+      //应收金额需要自定义
+      //应收时间需要自定义
+      //创建人员需要自定义 F
+      //备注需要自定义
+      receivadles.setUncollectedamount(constract.getAmount() - receivadles.getAmountreceived());
+      receivadles.setCreatetime(new Date());//改long？
+      receivadlesMapper.insert(receivadles);
+    }
+  }
+
+  @Override
+  @Transactional
+  public void deleteReceivablesById(Integer id) throws CustomerException {
+    Receivadles receivadles = receivadlesMapper.selectByPrimaryKey(id);
+    if(receivadles == null){
+      throw new CustomerException("要删除的应收账款不存在");
+    }
+    receivadlesMapper.deleteByPrimaryKey(id);
+  }
+
+  @Override
+  public void batchDelete(Integer[] ids) throws CustomerException {
+    for(Integer id : ids){
+      this.deleteReceivablesById(id);
+    }
+  }
+
+  @Override
+  public List<Receivadles> selectAll() {
+    return receivadlesMapper.selectByExample(new ReceivadlesExample());
+  }
+}
